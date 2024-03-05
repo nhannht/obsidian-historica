d1ac8a4f7ee8b45004b05808353b8cf1832a58fe
nhannht <nhanclassroom@gmail.com>
2024-03-06 02:38:42 +0700

    feat: add BCE and AD pattern to time recognize pattern

--- src/setupCustomChrono.ts
+++ src/setupCustomChrono.ts
@@ -1,5 +1,21 @@
 import * as chrono from "chrono-node";
 
+const BCEpattern: RegExp[] = [
+	/\b(\d{4})\s+B\.?C\.?E?\.?\b/i,
+	/\b(\d{4})\s+Before\s+\w{1,2}\s+Christ\b/i,
+	/\b(\d{4})\s+Before\s+\w{1,2}\s+Common\s+Era\b/i,
+	/\bB\.?C\.?E?\.?:?\s+(\d{4})\b/i,
+	/\bBefore\s+\w{1,2}\s+Christ\s+(\d{4})\b/i,
+	/\bBefore\s+\w{1,2}\s+Common\s+Era\s+(\d{4})\b/i,
+]
+
+const ADpattern: RegExp[] = [
+	/\b(\d{4})\s+A\.?D\.?E?\.?\b/i,
+	/\b(\d{4})\s+Anno\s+Domini\b/i,
+	/\bA\.?D\.?E?\.?:?\s+(\d{4})\b/i,
+	/\bAnno\s+Domini\s+(\d{4})\b/i,
+]
+
 export async function setupCustomChrono() {
 	const customChrono = chrono.casual.clone()
 	customChrono.parsers.push({
@@ -12,5 +28,32 @@ export async function setupCustomChrono() {
 			}
 		}
 	})
+
+	BCEpattern.forEach((pattern) => {
+		customChrono.parsers.push({
+			pattern: () => {
+				return pattern
+			},
+			extract: (context, match) => {
+				return {
+					year: -parseInt(match[1])
+				}
+			}
+		})
+	})
+	ADpattern.forEach((pattern) => {
+		customChrono.parsers.push({
+			pattern: () => {
+				return pattern
+			},
+			extract: (context, match) => {
+				return {
+					year: parseInt(match[1])
+				}
+			}
+		})
+	})
+
+
 	return customChrono
 }
