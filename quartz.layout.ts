31fbedeaad54a2e837ed783c27629223e3e550d3
nhannht <nhanclassroom@gmail.com>
2024-10-28 12:54:33 +0700

    doc: update document

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -37,7 +37,7 @@ export const defaultContentPageLayout: PageLayout = {
 		Component.DesktopOnly(Component.TableOfContents()),
 		Component.Backlinks(),
 	],
-	footer: Component.Discussion()
+	// footer: Component.Discussion()
 
 
 }
